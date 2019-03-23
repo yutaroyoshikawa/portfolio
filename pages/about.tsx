6558ceeb4d29f2ce1df5f3e708018333a548c03d
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-03-23 17:46:29 +0900

    Add src directory

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import Link from "next/link";
-import Header from "../components/header";
+import Header from "../src/components/header";
 
 interface IProps {
   isServer: string
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Header from "../components/header";
+import Header from "../src/components/header";
 
 function Index() {
   return (
