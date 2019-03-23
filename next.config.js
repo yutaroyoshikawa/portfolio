49b86db1a33e39c9d5a335fe76ff485f51447e49
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-03-23 18:17:27 +0900

    Add dotenv

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const withTypescript = require("@zeit/next-typescript");
+const withTypescript = require("@zeit/next-typescript")
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
 
 module.exports = withTypescript({
   target: 'serverless',
-  poweredByHeader: false
+  poweredByHeader: false,
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+
+    return config
+  }
 })
