1eaeacf990d3116aed3d10f0fbc50b693ef5e400
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-04-25 15:42:53 +0900

    add contact saga to index

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,11 +1,9 @@
 import { SagaIterator } from 'redux-saga'
 import { all } from 'redux-saga/effects';
 import menu from './menu'
-import contact from './contact'
 
 export default function* (): SagaIterator{
   yield all([
     ...menu,
-    ...contact,
   ])
 }
