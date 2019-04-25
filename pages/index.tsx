660f199d84c41e4fa4e091a2e3d0c2faf2a23ac1
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-04-25 15:44:56 +0900

    get works from apollo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,77 @@
-import Link from 'next/link'
-import Header from '../src/components/header'
 import * as Styled from '../src/styles/index'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import { Works } from '../src/components'
 
-function Index() {
+export default () => {
+  const GET_WORKS = gql`
+    {
+      workses {
+        title
+        thumb
+      }
+    }
+  `
+
+  interface IWorks {
+    title: string
+    thumb: string
+  }
+
+  interface IQuery {
+    workses: IWorks[]
+  }
+
+  const renderLoading = () => (
+    <Styled.MainContent>
+      <Styled.LoadingBar>
+        <Styled.LoadingFirstBar />
+        <Styled.LoadingSecondBar />
+        <Styled.LoadingThirdBar />
+      </Styled.LoadingBar>
+    </Styled.MainContent>
+  )
+
+  const renderError = () => (
+    <Styled.Error>
+      <p>Error</p>
+    </Styled.Error>
+  )
+
+  const renderMain = (data: IQuery, isLoading: boolean) => (
+    <div>
+    <Styled.Loading
+      in={isLoading}
+      timeout={300}
+    >
+      <Styled.LoadingBar>
+        <Styled.LoadingFirstBar />
+        <Styled.LoadingSecondBar />
+        <Styled.LoadingThirdBar />
+      </Styled.LoadingBar>
+    </Styled.Loading>
+    {
+      !isLoading ?
+        <Styled.MainContent>
+          {
+            data.workses.map(data => <Works title={data.title} thumb={data.thumb} />)
+          }
+        </Styled.MainContent>
+        :
+        null
+    }
+    </div>
+  )
+
   return (
-    <main>
-      <Styled.Entire>
-        <Header />
-        <section>
-          <Link href="/about">
-            <a>Go to About Me</a>
-          </Link>
-          {/* <Works /> */}
-        </section>
-      </Styled.Entire>
-    </main>
-  );
+    <Styled.Entire>
+      <Query query={GET_WORKS}>
+        {({loading, error, data}) => {
+          if (loading) return renderLoading()
+          if (error) return renderError()
+          return renderMain(data, loading)
+        }}
+      </Query>
+    </Styled.Entire>
+  )
 }
-
-export default Index;
